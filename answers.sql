2b689ed2a6f79ed2e60cbcc4d70c8ea49acf32df
Bossy-V-Osinde <vivianbosibori029@gmail.com>
2025-04-22 20:40:22 +0300

    Normalization project

---
+++ answers.sql
@@ -0,0 +1,67 @@
+-- Question 1 Achieving 1NF (First Normal Form)
+-- Create the Orders table
+CREATE TABLE Orders (
+    OrderID INT AUTO_INCREMENT PRIMARY KEY ,
+    CustomerName VARCHAR(150)
+);
+
+-- Create the OrderProducts table
+CREATE TABLE OrderProducts (
+    OrderProductID INT AUTO_INCREMENT PRIMARY KEY,
+    OrderID INT,
+    Product VARCHAR(100),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+
+-- Insert data into the Orders table
+INSERT INTO Orders (OrderID, CustomerName)
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
+
+-- Insert data into the OrderProducts table
+INSERT INTO OrderProducts (OrderID, Product)
+VALUES
+(101, 'Laptop'),
+(101, 'Mouse'),
+(102, 'Tablet'),
+(102, 'Keyboard'),
+(102, 'Mouse'),
+(103, 'Phone');
+
+-- Question 2 Achieving 2NF (Second Normal Form)
+-- Create the Orders table
+CREATE TABLE Orders (
+    OrderID INT AUTO_INCREMENT PRIMARY KEY ,
+    CustomerName VARCHAR(150)
+);
+
+-- Create the OrderProducts table
+-- Create the OrderProducts table with OrderProductID as the primary key
+CREATE TABLE OrderProducts (
+    OrderProductID INT AUTO_INCREMENT PRIMARY KEY,
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+
+-- Insert data into the Orders table
+INSERT INTO Orders (OrderID, CustomerName)
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
+
+-- Insert data into the OrderProducts table
+INSERT INTO OrderProducts (OrderID, Product, Quantity)
+VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
